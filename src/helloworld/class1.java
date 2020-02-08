61e2111791333c58351996e8bb50ddbcda3944de
Hiba Ghanim <hibaghanim5@gmail.com>
2020-02-08 18:40:40 +0200

    second commit

--- src/helloworld/class1.java
+++ src/helloworld/class1.java
@@ -5,7 +5,7 @@ public class class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("hello world");
+		System.out.print("hello Hiba!");
 	}
 
 }
